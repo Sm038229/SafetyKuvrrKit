002c44fefcf1ba7e1e39421a03c533deaed54941
Sachin Mishra <sachchida@netsutra.com>
2024-06-28 23:22:30 +0530

    fixes

--- SafetyKuvrrKit/SafetyKuvrrKit/SKTesting.swift
+++ SafetyKuvrrKit/SafetyKuvrrKit/SKTesting.swift
@@ -14,7 +14,7 @@ public struct SKTesting {
     }
     
     private static func sessionAPI() {
-        SKService.apiCall(with: "https://safety-red5.kuvrr.com/api/v1/session_init/", responseModel: SKDataResponse.self) { response in
+        SKService.apiCall(with: "https://safety-red5.kuvrr.com/api/v1/session_init/", responseModel: SKCSRFToken.self) { response in
             guard let response = response else { return }
             print("Success: \(response)")
         } failure: { error in
@@ -39,5 +39,15 @@ struct SKDataResponse: Decodable {
     let id: Int
     let userId: Int
     let title: String
+}
+
+struct SKCSRFToken: Decodable {
+    let token: String
+    let message: String
+    
+    enum CodingKeys: String, CodingKey {
+        case token = "csrf_token"
+        case message = "message"
+    }
 }
 
